28879a667613490b96de2a95d49eb5589b525664
Davronbek <davsattor@gmail.com>
2022-05-04 01:46:09 +0200

    Changes

--- src/data.js
+++ src/data.js
@@ -20,13 +20,13 @@ export const projects = [
         type : 'old'
     },
     {
-        name : 'Landing Page',
-        link : 'http://krayrpol.pl/',
+        name : 'To do List',
+        link : 'https://stunning-dango-219a4f.netlify.app/',
         type : 'old'
     },
     {
-        name : 'Restaurant project',
-        link : 'https://habibi-food-second-version.netlify.app/',
-        type : 'new'
+        name : 'Habibi Food',
+        link : 'https://jolly-easley-696d89.netlify.app',
+        type : 'old'
     },
 ]
