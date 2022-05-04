a93a926125438478d8ce30d42b2fcb066ca1a7f9
Davronbek <davsattor@gmail.com>
2022-05-04 16:35:09 +0200

    Style

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -41,7 +41,7 @@ const Projects = () => {
                                 <div className='project-single'>
                                     <a href={el.link} target="_blank">{el.name}</a>
 
-                                    <a href="" className='project-github'>
+                                    <a href={el.githubLink} className='project-github'>
                                        <img src={github} alt=""/>
                                     </a>
                                 </div>
--- src/data.js
+++ src/data.js
@@ -2,21 +2,25 @@ export const projects = [
     {
         name : 'Landing Page',
         link : 'http://krayrpol.pl/',
+        githubLink : 'https://github.com/DavronbekSattorov/cohort',
         type : 'new'
     },
     {
         name : 'Restaurant project',
         link : 'https://habibi-food-second-version.netlify.app/',
+        githubLink : 'https://github.com/DavronbekSattorov/habibi-food2',
         type : 'new'
     },
     {
         name : 'To do List',
         link : 'https://stunning-dango-219a4f.netlify.app/',
+        githubLink : 'https://github.com/DavronbekSattorov/react-todo-list',
         type : 'old'
     },
     {
         name : 'Habibi Food',
         link : 'https://jolly-easley-696d89.netlify.app',
+        githubLink : 'https://github.com/DavronbekSattorov/habibi-food',
         type : 'old'
     },
 {
