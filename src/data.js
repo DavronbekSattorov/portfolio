158bb846d9d495e1a908715c618d80cd58b1f97b
Davronbek <davsattor@gmail.com>
2022-05-04 14:10:56 +0200

    Chnages

--- src/data.js
+++ src/data.js
@@ -10,16 +10,6 @@ export const projects = [
         type : 'new'
     },
     {
-        name : 'Landing Page',
-        link : 'http://krayrpol.pl/',
-        type : 'new'
-    },
-    {
-        name : 'Restaurant project',
-        link : 'https://habibi-food-second-version.netlify.app/',
-        type : 'old'
-    },
-    {
         name : 'To do List',
         link : 'https://stunning-dango-219a4f.netlify.app/',
         type : 'old'
@@ -27,6 +17,18 @@ export const projects = [
     {
         name : 'Habibi Food',
         link : 'https://jolly-easley-696d89.netlify.app',
+        type : 'old'
+    },
+{
+        name : 'Junk Food',
+        link : 'https://sprightly-lily-cd407f.netlify.app/',
+        githubLink: 'https://github.com/DavronbekSattorov/shopName',
+        type : 'old'
+    },
+{
+        name : 'Marketing',
+        link : 'https://sprightly-lily-cd407f.netlify.app/',
+        githubLink: 'https://github.com/DavronbekSattorov/spacdyna',
         type : 'old'
     },
 ]
